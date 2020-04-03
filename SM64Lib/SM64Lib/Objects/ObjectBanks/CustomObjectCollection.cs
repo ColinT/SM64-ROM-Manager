f0de3a0062f7441fdbdafd2c344011a6895a4b93
Pilzinsel64 <pilzinsel64@gmx.de>
2020-04-03 10:12:21 +0200

    add classes for custom objects

---
+++ SM64Lib/SM64Lib/Objects/ObjectBanks/CustomObjectCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SM64Lib.Objects.ObjectBanks
+{
+    public class CustomObjectCollection
+    {
+        public List<CustomObject> CustomObjects { get; } = new List<CustomObject>();
+    }
+}
