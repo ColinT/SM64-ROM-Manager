f61d595555498dbc5156bd8b3607aed1f0785508
Pilzinsel64 <pilzinsel64@gmx.de>
2020-04-26 17:38:40 +0200

    make GUI design for Custom ASM Codes integration

---
+++ SM64 ROM Manager/RomManager/GUI/Tools/Custom Asm Codes/CustomAsmCodesManager.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevComponents.DotNetBar;
+
+namespace SM64_ROM_Manager
+{
+    public partial class CustomAsmCodesManager : DevComponents.DotNetBar.OfficeForm
+    {
+        public CustomAsmCodesManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
